adc33d5faade1c5012dd3fc78169c6bb78afdddf
Taisuke Fukuno <fukuno@jig.jp>
2024-10-22 17:31:56 +0900

    add 評価点合計計算

--- calcStress.js
+++ calcStress.js
@@ -9,7 +9,7 @@ stresscheck.forEach(i => {
   i.category = i.category1[0];
 });
 
-const calcurl = "./stresscheck-calctable.csv";
+const calcurl = "./stresscheck-calctable.csv?";
 const calctable = await CSV.fetchJSON(calcurl);
 
 export const calcStress = (stresssurvey) => { // [1-4]*57
@@ -23,18 +23,27 @@ export const calcStress = (stresssurvey) => { // [1-4]*57
       }
     }
     const point = calcPoint(calc.計算式, n);
-    const o = { 尺度: calc.尺度, ポイント: point };
+    const o = { カテゴリ: calc.カテゴリ, 尺度: calc.尺度 }; //, ポイント: point };
     for (const c of "男女") {
       let rank = 0;
       for (let i = 1; i <= 5; i++) {
         if (point <= parseInt(calc[c + "性" + i])) {
-          rank = i;
+          rank = parseInt(calc.評価点[i - 1]);
           break;
         }
       }
-      o[c + "性（高さ/多さ）"] = rank;
+      //o[c + "性（高さ/多さ）"] = rank;
+      o[c + "性 評価点"] = rank;
     }
     res.push(o);
+  }
+  for (const cate of "CBA") {
+    const o = { カテゴリ: cate, 尺度: "領域" + cate + " 合計" };
+    for (const c of "男女") {
+      const sum = res.filter(i => i.カテゴリ == cate).reduce((pre, cur) => pre + cur[c + "性 評価点"], 0);
+      o[c + "性 評価点"] = sum;
+    }
+    res.unshift(o);
   }
   return res;
 };
